cbf1cd7bef366bec8f7b92c486fc1fabc2c10d7a
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-03-19 09:21:15 +0700

    updates

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from modules.filters import KalmanFilter1D
 if __name__ == "__main__":
     cam = Vision(True)
     rt = Routine()
-    rt.load("assets/data/data.weights",
-            "assets/config/config.cfg",
-            "assets/class/class.names")
+    rt.load("assets/data/tiny.weights",
+            "assets/config/tiny.cfg",
+            "assets/class/coco.names")
     while True:
         frame = cam.read(frame_size=480, show_fps=True)
-        detect = rt.get(frame)
+        detect = rt.get(frame=frame)
+        rt.draw(frame=frame, detection=detect)
         cam.show(frame, "frame")
         cam.wait(1)
