c485935bdae4ac21fe85b82b12366549658448ab
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-03-24 20:52:44 +0700

    Updated ros

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from modules.utils import *
 from modules.process import Routine
 from modules.image import Vision
 from modules.filters import KalmanFilter1D
+import rospy
 
-# init
-cam = Vision(True)
-rt = Routine()
-rt.load("assets/data/tiny.weights",
-        "assets/config/tiny.cfg",
-        "assets/class/coco.names")
-
-
-def main():  # loop
-    frame = cam.read(frame_size=480, show_fps=True)
-    detect = rt.get(frame=frame)
-    print(f"detect : {detect}")
-    rt.draw(frame=frame, detection=detect)
-    cam.show(frame, "frame")
-    cam.wait(1)
+if __name__ == "__main__":
+    cam = Vision(True)
+    rt = Routine()
+    rt.load("assets/data/tiny.weights",
+            "assets/config/tiny.cfg",
+            "assets/class/coco.names")
+    try:
+        rospy.init_node("cat-fur", anonymous=True)
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            frame = cam.read(frame_size=480, show_fps=True)
+            detect = rt.get(frame=frame)
+            print(f"detect : {detect}")
+            rt.draw(frame=frame, detection=detect)
+            cam.show(frame, "frame")
+            cam.wait(1)
+    except rospy.ROSInterruptException:
+        pass
---
+++ utils/node.py
@@ -0,0 +1,21 @@
+import rospy
+
+
+class Data:
+    def __init__(self):
+        pass
+
+
+class Subscriber():
+    def __init__(self):
+        pass
+
+
+class Publisher():
+    def __init__(self):
+        pass
+
+
+class NodeHandler(object):
+    def __init__(self):
+        pass
