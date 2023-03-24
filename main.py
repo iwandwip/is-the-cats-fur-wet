6c1d4277755c3c6f9540e47cc0a25c61e1a20469
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-03-24 20:40:44 +0700

    Updated

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from modules.process import Routine
 from modules.image import Vision
 from modules.filters import KalmanFilter1D
 
+# init
 cam = Vision(True)
 rt = Routine()
 rt.load("assets/data/tiny.weights",
@@ -10,11 +11,10 @@ rt.load("assets/data/tiny.weights",
         "assets/class/coco.names")
 
 
-def main():
-    while True:
-        frame = cam.read(frame_size=480, show_fps=True)
-        detect = rt.get(frame=frame)
-        print(f"detect : {detect}")
-        rt.draw(frame=frame, detection=detect)
-        cam.show(frame, "frame")
-        cam.wait(1)
+def main():  # loop
+    frame = cam.read(frame_size=480, show_fps=True)
+    detect = rt.get(frame=frame)
+    print(f"detect : {detect}")
+    rt.draw(frame=frame, detection=detect)
+    cam.show(frame, "frame")
+    cam.wait(1)
--- node_handler.py
+++ node_handler.py
@@ -1,6 +1,20 @@
 import rospy
+import main
 
 
 class NodeHandler(object):
     def __init__(self):
         rospy.init_node("cat-fur", anonymous=True)
+        self.rate = rospy.Rate(10)  # 10hz
+
+    def run(self):
+        try:
+            while not rospy.is_shutdown():
+                main.main()
+        except rospy.ROSInterruptException:
+            pass
+
+
+if __name__ == "__main__":
+    nh = NodeHandler()
+    nh.run()
