3cb576fa8754d2c335584c8a7d5021e6a9828faf
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-05-18 11:18:27 +0700

    updates

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from modules.routine import ImgBuster
 # import rospy
 
 if __name__ == "__main__":
-    cam = Vision(False, "core/cat.mp4")
+    cam = Vision(False, "core/wet-cat.mp4")
     pr = ImgBuster()
     pr.load("assets/class/cats.txt", "assets/data/best.pt")
     try:
         while True:
             frame = cam.read(480, True)
             detect = pr.predict(frame)
+            for i, data in enumerate(detect):
+                print(detect[i]['class'])
             pr.draw(frame, detect)
             cam.show(frame, "frame")
             cam.wait(1)
