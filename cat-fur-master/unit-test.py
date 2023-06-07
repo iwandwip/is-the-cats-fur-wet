524912da3f8511625dad0d18a0f3e2c1159e992d
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-06-07 22:39:25 +0700

    updates

--- cat-fur-master/unit-test.py
+++ cat-fur-master/unit-test.py
@@ -4,15 +4,13 @@ from modules.filters import KalmanFilter
 from modules.routine import ImgBuster as Yolo
 
 if __name__ == "__main__":
-    cam = Vision(isUsingCam=True, addr="men.mp4")
+    cam = Vision(isUsingCam=False, addr="video/wet-cat.mp4")
     pr = Yolo()
-    pr.load("assets/class/coco.txt", "assets/data/yolov5s.pt")
+    pr.load("assets/class/cats.txt", "assets/data/cats.pt")
     try:
         while True:
             frame = cam.read(480, True)
             detect = pr.predict(frame)
-            for i, data in enumerate(detect):
-                print(data)
             pr.draw(frame, detect)
             cam.show(frame, "frame")
             if cam.wait(1) == ord('q'):
