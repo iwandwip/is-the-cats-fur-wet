e1e22ffdb96281d3d7f2d97a68a557a90b5fb406
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2023-06-08 11:03:22 +0700

    Update filters.py

--- cat-fur-master/modules/filters.py
+++ cat-fur-master/modules/filters.py
@@ -2,6 +2,62 @@ import cv2
 import numpy as np
 
 
+class SimpleKalmanFilter:
+    """
+    CARA PENGGUNAAN:
+        while True:
+        estimate = kf.update_estimate(new_measurement)
+        print(f"Estimasi baru: {estimate}")
+
+        # Mendapatkan gain Kalman saat ini
+        gain = kf.get_kalman_gain()
+        print(f"Gain Kalman saat ini: {gain}")
+        
+        # Mengubah kesalahan pengukuran
+        kf.set_measurement_error(0.5)
+        
+        # Mengubah kesalahan estimasi
+        kf.set_estimate_error(0.2)
+        
+        # Mengubah noise proses
+        kf.set_process_noise(0.05)
+        
+        # Mendapatkan kesalahan estimasi setelah perubahan
+        err_estimate = kf.get_estimate_error()
+        print(f"Kesalahan estimasi setelah perubahan: {err_estimate}")
+    """
+    def __init__(self, mea_e, est_e, q):
+        self._err_measure = mea_e
+        self._err_estimate = est_e
+        self._q = q
+        self._current_estimate = 0
+        self._last_estimate = 0
+        self._kalman_gain = 0
+
+    def update_estimate(self, mea):
+        self._kalman_gain = self._err_estimate / (self._err_estimate + self._err_measure)
+        self._current_estimate = self._last_estimate + self._kalman_gain * (mea - self._last_estimate)
+        self._err_estimate = (1.0 - self._kalman_gain) * self._err_estimate + abs(self._last_estimate - self._current_estimate) * self._q
+        self._last_estimate = self._current_estimate
+
+        return self._current_estimate
+
+    def set_measurement_error(self, mea_e):
+        self._err_measure = mea_e
+
+    def set_estimate_error(self, est_e):
+        self._err_estimate = est_e
+
+    def set_process_noise(self, q):
+        self._q = q
+
+    def get_kalman_gain(self):
+        return self._kalman_gain
+
+    def get_estimate_error(self):
+        return self._err_estimate
+
+
 class KalmanFilter:
     def __init__(self, initial_state, initial_covariance, measurement_noise, process_noise):
         # Inisialisasi matriks sistem dan ketidakpastian pada sensor dan model sistem
